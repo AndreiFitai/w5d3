90b64c82aac80a5413aead2fa665528a0aaa6e91
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-08-29 15:59:59 +0200

    added put

--- routes/api.js
+++ routes/api.js
@@ -37,8 +37,43 @@ router.patch('/food/:index', (req, res) => {
     })
 })
 
-router.get('/food', (req, res) => {}) // list of foods
+router.get('/food', (req, res) => {
+    Food.find({}).then(foods => {
+        res.send(foods)
+    })
+})
 
-router.get('/food/:index', (req, res) => {}) // just one food
+router.get('/food/:index', (req, res) => {
+    Food.findOne({ index: req.params.index }).then(food => {
+        if (food) return res.send(food)
+        else res.send({ err: 404 })
+    })
+})
+
+router.put('/food/:index', (req, res) => {
+    const { title, cuisine, calories, type } = req.body
+
+    if (!title || !cuisine || !calories || !type) {
+        return res.status(400).send({ error: 'Please provide all required fields.' })
+    }
+
+    const { index } = req.params
+
+    Food.findOne({ index })
+        .then(currentFood => {
+            if (currentFood) {
+                return Food.findOneAndUpdate(
+                    { index },
+                    { title, cuisine, calories, type },
+                    { new: true }
+                )
+            } else {
+                return new Food({ title, cuisine, calories, type, index }).save()
+            }
+        })
+        .then(newFood => {
+            res.send(newFood)
+        })
+})
 
 module.exports = router
